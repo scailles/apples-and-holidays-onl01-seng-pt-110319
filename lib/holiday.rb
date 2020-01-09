407787dcfff7631ab384264a654a4ee9c1229c20
Shaina C <scailles23@gmail.com>
2020-01-09 10:55:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-array=
+array = holiday_hash.map 
 end
 
 def all_holidays_with_bbq(holiday_hash)

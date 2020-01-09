71e6ffdd868baa8950219c52865928df15860b9d
Shaina C <scailles23@gmail.com>
2020-01-09 11:50:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts "#{season.to_s.capitalize}: "
+  puts "#{season.to_s.capitalize}:"
   holiday.each do |holiday, supply|
     puts "#{holiday.to_s.gsub("  ", " ").split.map(&:capitalize).join(' ')}: #{supply.join(", ")}"
 end

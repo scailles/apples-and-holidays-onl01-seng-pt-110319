5f8d62f7e68feeb510b6713d2fc8e1b202f9690a
Shaina C <scailles23@gmail.com>
2020-01-09 10:28:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
 end
 end
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above

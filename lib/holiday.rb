f157d0c43ad4b3ac8271539a6271ef69efad436a
Shaina C <scailles23@gmail.com>
2020-01-09 11:00:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-array = holiday_hash[:winter].map do |winter, supply|
+array = holiday_hash[:winter].map do |season, supply|
    supply
  end
    return array.flatten

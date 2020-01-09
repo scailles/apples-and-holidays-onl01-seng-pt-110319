31e8657acf5eae7c62534f487c1661256ab7749c
Shaina C <scailles23@gmail.com>
2020-01-09 10:43:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,8 +40,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash[:fall]["Columbus"] <<  add_new_holiday_with_supplies
-  return holiday_hash
+  holiday_hash[:fall]["Columbus"] <<  second_supply_for_fourth_of_july
 end
 
 def all_winter_holiday_supplies(holiday_hash)

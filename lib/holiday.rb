edb5cfc177e698ad4e2b7d567f44a61c40e0eb32
Shaina C <scailles23@gmail.com>
2020-01-09 11:44:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
+  holiday.each do |holiday, supply|
+    puts 
 end
 end
 
